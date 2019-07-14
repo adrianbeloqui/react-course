acd97c1651d6293164dd41767e82ade6186b4c05
Adrian Beloqui <adribeloqui@gmail.com>
2019-07-13 19:43:33 -0500

    Initial commit

---
+++ app-one/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Person from './Components/Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <Person />
+    </div>
+  );
+}
+
+export default App;
