47c4f82332df206e73e29e08bccc8a2fcc96cfed
Adrian Beloqui <adribeloqui@gmail.com>
2020-02-13 18:11:51 -0600

    Using PropTypes

--- app-one/src/Components/Persons/Person/Person.js
+++ app-one/src/Components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import classes from './Person.module.css'
 import Aux from '../../Hoc/Aux'
 import withClass from '../../Hoc/withClass'
@@ -6,11 +8,31 @@ import withClass from '../../Hoc/withClass'
 const person = (props) => {
     return (
         <Aux>
-            <p onClick={ props.click }>I'm a { props.name } and I am { props.age } years old</p>
+            <p onClick={ props.click }>
+                I'm a { props.name } and I am { props.age } years old
+            </p>
             <p>{ props.children }</p>
-            <input type="text" onChange={ props.change } value={ props.name }/>
+            <input
+                type="text"
+                onChange={ props.change }
+                value={ props.name }
+            />
         </Aux>
     )
+}
+
+person.propTypes = {
+    'click': PropTypes.func,
+    'change': PropTypes.func,
+    'name': PropTypes.string,
+    'age': PropTypes.number
+}
+
+person.defaultTypes = {
+    'click': null,
+    'change': null,
+    'name': '',
+    'age': 0
 }
 
 export default withClass(person, classes.Person);
