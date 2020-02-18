c6597082ef343fe62c61dbbf766b9e390ea50dd8
Adrian Beloqui <adribeloqui@gmail.com>
2020-02-17 19:07:46 -0600

    Initial setup for the burger builder app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Code from React course
+
+www.udemy.com/course/react-the-complete-guide-incl-redux
---
+++ burger-builder/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from './components/Layout/Layout'
+
+
+function App() {
+  return (
+    <div>
+      <Layout >
+        Test
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
