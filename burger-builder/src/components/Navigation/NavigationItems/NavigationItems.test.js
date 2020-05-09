db9a01423c9d2fee2d527dbb81465dc4714fda0a
Adrian Beloqui <adribeloqui@gmail.com>
2020-05-08 19:16:43 -0500

    Section : Testing

---
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import NavigationItems from './NavigationItems'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+
+configure({ adapter: new Adapter() })
+
+describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    })
+
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
+    })
+
+    it('should render Logout <NavigationItem /> element if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
+    })
+
+    it('should not render Authenticate <NavigationItem /> element if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link="/auth">Authenticate</NavigationItem>)).toEqual(false)
+    })
+})
